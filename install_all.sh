7f5f0f6ea880c390d31231b1fbb0a48ff96a6c24
Pitymivo <68125027+Paul2009-38@users.noreply.github.com>
2023-01-06 12:00:28 +0100

    Update install_all.sh

--- install_all.sh
+++ install_all.sh
@@ -4,4 +4,4 @@ unzip irrlicht.zip
 unzip minetest_game.zip
 mv minetest-5.6.1 minetest
 mv irrlicht-1.9.0mt9 minetest/lib/irrlichtmt
-mv minetest_game minetest/games/
+mv minetest_game-5.6.1 minetest/games/minetest_game
