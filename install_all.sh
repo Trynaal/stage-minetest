b0f4425f83d0c964467bf990690970b8375c2990
Paul <Paul>
2023-01-06 18:40:36 +0100

    update install_all.sh

--- install_all.sh
+++ install_all.sh
@@ -6,3 +6,7 @@ mv minetest-5.6.1 minetest
 mv irrlicht-1.9.0mt9 minetest/lib/irrlichtmt
 mv minetest_game-5.6.1 minetest/games/minetest_game
 mv mods minetest/games/minetest_games/mods/
+sudo apt install -y g++ make libc6-dev cmake libpng-dev libjpeg-dev libxi-dev libgl1-mesa-dev libsqlite3-dev libogg-dev libvorbis-dev libopenal-dev libcurl4-gnutls-dev libfreetype6-dev zlib1g-dev libgmp-dev libjsoncpp-dev libzstd-dev libluajit-5.1-dev
+cd minetest
+cmake . -DRUN_IN_PLACE=TRUE -DBUILD_SERVER=TRUE -DBUILD_CLIENT=FALSE -DENABLE_CURL=ON -DENABLE_LUAJIT=ON .
+make -j$(nproc)
